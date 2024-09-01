71a58e519093f6ed25860c197b8ca52b308205fa
Kobby <Kobby.amos16@gmail.com>
2024-09-01 18:13:32 +0100

    added a new repo

--- form-handling-react/alx-react-app/src/App.jsx
+++ form-handling-react/alx-react-app/src/App.jsx
@@ -2,10 +2,14 @@ import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
-import TodoList from './components/TodoList';
+import WelcomeMessage from './components/WelcomeMessage';
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
+import UserProfile from './components/UserProfile';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -17,10 +21,11 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <div className="App">
-      <TodoList />
-    </div>
-      <h1>Vite + React</h1>
+      <WelcomeMessage />
+      <Header />
+      <MainContent />
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+      <Footer />
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -33,7 +38,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
